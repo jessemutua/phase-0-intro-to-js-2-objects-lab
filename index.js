c369aafade6802b9bb55d05a471561ff37dca106
Jesse <jessemutua76@gmail.com>
2022-11-25 13:50:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    Name: "Jesse",
+    streetAddress: "1234",
+}
+
+function beforeEach() {
+    for (const key in employee) {
+        delete employee[key];
+    }
+    employee.employeeName = "Sam";
+
+}
+
+beforeEach();
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    employee = {
+        ...employee,
+        streetAddress: value
+    }
+    return employee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    employee[streetAddress] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    employee = {
+        ...employee,
+    }
+    delete employee[key]
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
